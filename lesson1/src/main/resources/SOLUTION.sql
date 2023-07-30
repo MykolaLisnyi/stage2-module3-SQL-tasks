008c4d8bb4f309cabef443d721b9167b2fd3b0c2
Mykola Lisnyi <lesnoy.nikolay@gmail.com>
2023-07-30 15:58:47 +0300

    4

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,5 +1,5 @@
-CREATE TABLE student(id BIGINT PRIMARY KEY, name VARCHAR, birthday DATE, groupnumber INT);
-CREATE TABLE subject(id BIGINT PRIMARY KEY, name VARCHAR, description VARCHAR, grade INT);
-CREATE TABLE paymentType(id BIGINT PRIMARY KEY, name VARCHAR);
-CREATE TABLE payment(id BIGINT PRIMARY KEY, type_id BIGINT, amount DECIMAL, student_id BIGINT, payment_date DATETIME, FOREIGN KEY (type_id) REFERENCES paymentType(type_id), FOREIGN KEY (student_id) REFERENCES student(student_id));
-CREATE TABLE mark(id BIGINT PRIMARY KEY, student_id BIGINT, subject_id BIGINT, mark INT, FOREIGN KEY (student_id) REFERENCES student(student_id), FOREIGN KEY (subject_id) REFERENCES subject(subject_id));
+CREATE TABLE student(id INT PRIMARY KEY, name VARCHAR, birthday DATE, groupnumber INT);
+CREATE TABLE subject(id INT PRIMARY KEY, name VARCHAR, description VARCHAR, grade INT);
+CREATE TABLE paymentType(id INT PRIMARY KEY, name VARCHAR);
+CREATE TABLE payment(id INT PRIMARY KEY, type_id INT, amount DECIMAL, student_id INT, payment_date DATETIME, FOREIGN KEY (type_id) REFERENCES paymentType(type_id), FOREIGN KEY (student_id) REFERENCES student(student_id));
+CREATE TABLE mark(id INT PRIMARY KEY, student_id INT, subject_id INT, mark INT, FOREIGN KEY (student_id) REFERENCES student(student_id), FOREIGN KEY (subject_id) REFERENCES subject(subject_id));
--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-DELETE student FROM student AS s INNER JOIN mark AS m ON s.id = m.student_id INNER JOIN subject AS sbj ON m.subject_id = sbj.id GROUP BY s.id HAVING MAX(sbj.grade) > 4;
-DELETE student FROM student AS s INNER JOIN mark AS m ON s.id = m.student_id GROUP BY s.id HAVING MIN(m.grade) < 4;
+DELETE FROM student AS s INNER JOIN mark AS m ON s.id = m.student_id INNER JOIN subject AS sbj ON m.subject_id = sbj.id GROUP BY s.id HAVING MAX(sbj.grade) > 4;
+DELETE FROM student AS s INNER JOIN mark AS m ON s.id = m.student_id GROUP BY s.id HAVING MIN(m.grade) < 4;
 DELETE FROM paymenttype WHERE name = 'DAILY';
 DELETE FROM mark WHERE mark < 7;
